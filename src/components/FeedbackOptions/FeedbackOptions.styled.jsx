682997f6e81a7e47a66875352f538bffa079c68e
GeloPerk <114014987+GeloPerk@users.noreply.github.com>
2023-03-30 12:03:38 +0300

    lets try

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  cursor: pointer;
+`;
+
+export const DivBtns = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  
+`;
