fabc9ffc306189d0aa6c71836b9e8b175d9522a6
GeloPerk <114014987+GeloPerk@users.noreply.github.com>
2023-03-30 12:52:56 +0300

    border

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -4,6 +4,6 @@ export const Div = styled.div`
     width: 600px;
     margin: 100px auto 0;
     padding: 20px;
-    border: 1px solid black;
+    border: 5px solid black;
     border-radius: 15px;
 `
